774c1a11a6bd094f1fd5b3423f4c705d1ff09e88
Jake Yeo <jyeo03@students.cs.ubc.ca>
2024-06-12 16:26:41 -0700

    Created API backend with MVC architecture with basic delete, update, add functionalities

---
+++ zenCafeBackend/routes/productRoute.ts
@@ -0,0 +1,21 @@
+const express = require("express");
+const Product = require("../models/product.model.ts");
+const router = express.Router();
+const controller = require('../controllers/productController');
+
+// create a product
+router.post('/', controller.createProduct);
+
+// get all products
+router.get('/', controller.getAllProducts);
+
+// get product by id
+router.get('/:id', controller.getProductById);
+
+// update a product
+router.put('/:id', controller.updateProductById);
+
+// delete a product
+router.delete('/:id', controller.deleteProductById);
+
+module.exports = router;
