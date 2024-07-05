54f54aec5375d3b2aef6af8d7e7b4a9507e1ce6a
Jake Yeo <jyeo03@students.cs.ubc.ca>
2024-07-04 22:54:13 -0700

    Added ability to create chatroom, fixed duplicate key bug.

--- src/functions/zenCafeChatroomsApi.ts
+++ src/functions/zenCafeChatroomsApi.ts
@@ -1,7 +1,14 @@
 import ChatroomMetadata from "../objects/ChatroomMetadata";
 
+function dataToChatroomMetadataObj(data: any): ChatroomMetadata {
+    const { _id, chatroomName, creatorUsername, creatorUid } = data;
 
-export function sendMessage(chatroom_id: string, senderUsername: string, senderUid: string, message: string): void {
+    const newChatroomMetadata = new ChatroomMetadata(chatroomName, creatorUsername, creatorUid, _id);
+
+    return newChatroomMetadata;
+}
+
+export function sendMessage(chatroom_id: string, senderUsername: string, senderUid: string, message: string): void { // remake this function
     fetch('http://localhost:3000/chatrooms/sendMessage', {
         method: 'PATCH',
         headers: {
@@ -19,6 +26,37 @@ export function sendMessage(chatroom_id: string, senderUsername: string, senderU
         .catch(error => console.error('Error:', error));
 }
 
+export async function createChatroom(chatroomName: string, creatorUsername: string, creatorUid: string): Promise<ChatroomMetadata | null> {
+    try {
+        const response = await fetch(`http://localhost:3000/chatrooms/createChatroom`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                chatroomName: chatroomName,
+                creatorUsername: creatorUsername,
+                creatorUid: creatorUid,
+            })
+        });
+
+        if (!response.ok) {
+            console.error('createChatroom Error:', 'Failed to fetch');
+            return null;
+        }
+
+        const data = await response.json();
+
+        const newChatroomMetadata = dataToChatroomMetadataObj(data);
+
+        return newChatroomMetadata;
+
+    } catch (error) {
+        console.error('getChatrooms Error:', 'Failed to fetch');
+        return null;
+    }
+}
+
 export async function getChatrooms(): Promise<ChatroomMetadata[] | null> {
     try {
         const response = await fetch(`http://localhost:3000/chatrooms/getChatrooms`, {
@@ -28,13 +66,17 @@ export async function getChatrooms(): Promise<ChatroomMetadata[] | null> {
             },
         });
 
+        if (!response.ok) {
+            console.error('getChatrooms Error:', 'Failed to fetch');
+            return null;
+        }
+
         const data = await response.json();
 
         var chatrooms: ChatroomMetadata[] = [];
 
         for (const jsonChatroomMetadata of data) {
-            const {_id, chatroomName, creatorUsername, creatorUid} = jsonChatroomMetadata;
-            chatrooms.push(new ChatroomMetadata(chatroomName, creatorUsername, creatorUid, _id));
+            chatrooms.push(dataToChatroomMetadataObj(jsonChatroomMetadata));
         }
 
         return chatrooms
--- src/pages/ChatroomsPage.tsx
+++ src/pages/ChatroomsPage.tsx
@@ -1,12 +1,13 @@
-import { Button, Dialog, Slide, Stack, Typography } from "@mui/material";
-import { getChatrooms } from "../functions/zenCafeChatroomsApi"
+import { Button, Dialog, Slide, Stack, TextField, Typography } from "@mui/material";
+import { createChatroom, getChatrooms } from "../functions/zenCafeChatroomsApi"
 import { JsxElement } from "typescript";
-import { ReactElement, useEffect, useState } from "react";
+import { ReactElement, useContext, useEffect, useState } from "react";
 import Chatroom from "../objects/Chatroom";
 import ChatroomMetadata from "../objects/ChatroomMetadata";
 import { useNavigate } from "react-router-dom";
 import React from "react";
 import { TransitionProps } from "@mui/material/transitions";
+import { SingletonUserContext } from "../firebase/FirebaseApi";
 const { v4: uuidv4 } = require('uuid');
 
 const Transition = React.forwardRef(function Transition( // make sure this is not in the element itself or it will constantly be set again and again which ruins the sliding close animation!
@@ -22,9 +23,13 @@ const ChatroomsPage = () => {
 
     const navigate = useNavigate();
 
+    const singletonUserContext = useContext(SingletonUserContext);
+
     const [isDialogueOpen, setIsDialogueOpen] = useState(false);
 
     const [chatrooms, setChatrooms] = useState<ChatroomMetadata[]>([]);
+
+    var chatName = "";
 
     useEffect(() => {
         const setChatroomsFromDB = async () => {
@@ -65,7 +70,8 @@ const ChatroomsPage = () => {
                 <Button onClick={(e) => {
                     setIsDialogueOpen(false);
                 }}>Close</Button>
-
+                <TextField onChange={(e) => { chatName = e.target.value }} id="outlined-basic" label="Outlined" variant="outlined" />
+                <Button onClick={() => {createChatroom(chatName, singletonUserContext.user.getUsername(), singletonUserContext.user.getGoogleId())}}>Create Chatroom</Button>
             </Dialog>
         </Stack>
     </>)
