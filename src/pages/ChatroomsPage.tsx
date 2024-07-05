f6d871367dbd07acbe10acddb001591aba68df3a
Jake Yeo <jyeo03@students.cs.ubc.ca>
2024-07-04 17:36:05 -0700

    Prototype for chatrooms display page

--- src/pages/ChatroomsPage.tsx
+++ src/pages/ChatroomsPage.tsx
@@ -1,11 +1,41 @@
+import { Stack, Typography } from "@mui/material";
+import { getChatrooms } from "../functions/zenCafeChatroomsApi"
+import { JsxElement } from "typescript";
+import { ReactElement, useEffect, useState } from "react";
+import Chatroom from "../objects/Chatroom";
+import ChatroomMetadata from "../objects/ChatroomMetadata";
 
 
 const ChatroomsPage = () => {
 
-    
+    const [chatrooms, setChatrooms] = useState<ChatroomMetadata[]>([]);
+
+    useEffect(() => {
+        const setChatroomsFromDB = async () => {
+            const chatrooms = await getChatrooms();
+
+            if (!chatrooms) {
+                console.error('ChatroomsPage Error:', 'Failed to get Chatrooms with getChatrooms()');
+                return;
+            }
+
+            setChatrooms(chatrooms);
+        }
+
+        setChatroomsFromDB();
+
+    }, []);
+
+    const typographyArray: ReactElement[] = [];
+
+    for (const chatroom of chatrooms) {
+        typographyArray.push(<Typography>{chatroom.getChatroomId()}</Typography>);
+    }
 
     return (<>
-
+        <Stack>
+            {typographyArray}
+        </Stack>
     </>)
 }
 
