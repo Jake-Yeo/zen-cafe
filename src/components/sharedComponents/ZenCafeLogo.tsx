54c13692d7af2385085bc176ba36dd00624315f3
Jake Yeo <jyeo03@students.cs.ubc.ca>
2024-07-11 00:25:21 -0700

    Added button styling, and better implementation of the ZenCafeLogo component

---
+++ src/components/sharedComponents/ZenCafeLogo.tsx
@@ -0,0 +1,80 @@
+import { Box } from "@mui/material";
+
+type props =
+    | { vw: number; vh?: never; horizontalLogo?: boolean }
+    | { vw?: never; vh: number; horizontalLogo?: boolean };
+
+// For the props above we used a union type or somthing. Basically, the user can either
+// A: pass in a vw, horizontalLogo, but not a vh
+// or
+// B: pass in a vh, horizontalLogo, but not a vw
+// I do this because I don't wan't to be able to give a vh and vw which would ruin the aspect ratio of the svg logo
+
+
+const ZenCafeLogo = ({ horizontalLogo = false, vw, vh }: props) => {
+
+
+    var zenCafeLogo = <></>;
+
+    if (!horizontalLogo) {
+        const aspectRatio = 374 / 399;
+
+        var localVh = 0;
+        var localVw = 0;
+
+        if (typeof vh === "undefined") {
+            localVw = vw;
+            localVh = vw * aspectRatio;
+        }
+
+        if (typeof vw === "undefined") {
+            localVh = vh;
+            localVw = vh * (1/aspectRatio);
+        }
+
+        zenCafeLogo =
+            <Box
+                sx={{
+                    backgroundImage: `url("/svg/ZenCafeVerticalLogo.svg")`, // Load background image
+                    backgroundSize: 'contain', // Scale the background image to fit within the container while preserving its aspect ratio
+                    backgroundRepeat: 'no-repeat',
+                    width: `${localVw}vw`, // Set the width of the container
+                    height: `${localVh}vh`, // Automatically adjust the height based on the aspect ratio
+                    zIndex: 1
+                }}
+            />
+    } else {
+        const aspectRatio = 254 / 645;
+
+        var localVh = 0;
+        var localVw = 0;
+
+        if (typeof vh === "undefined") {
+            localVw = vw;
+            localVh = vw * aspectRatio;
+        }
+
+        if (typeof vw === "undefined") {
+            localVh = vh;
+            localVw = vh * (1/aspectRatio);
+        }
+
+        zenCafeLogo =
+            <Box
+                sx={{
+                    backgroundImage: `url("/svg/ZenCafeHorizontalLogo.svg")`, // Load background image
+                    backgroundSize: 'contain', // Scale the background image to fit within the container while preserving its aspect ratio
+                    backgroundRepeat: 'no-repeat',
+                    width: `${localVw}vw`, // Set the width of the container
+                    height: `${localVh}vh`, // Automatically adjust the height based on the aspect ratio
+                    zIndex: 1
+                }}
+            />
+    }
+
+    return (<>
+        {zenCafeLogo}
+    </>)
+}
+
+export default ZenCafeLogo
--- src/pages/LoginSignupPage.tsx
+++ src/pages/LoginSignupPage.tsx
@@ -4,6 +4,7 @@ import { useContext } from "react";
 import { getChatrooms } from "../functions/zenCafeChatroomsApi";
 import { useNavigate, useRoutes } from "react-router-dom";
 import Background from "../components/sharedComponents/Background";
+import ZenCafeLogo from "../components/sharedComponents/ZenCafeLogo";
 const { v4: uuidv4 } = require('uuid');
 
 
@@ -15,24 +16,24 @@ const LoginSignupPage = () => {
 
     // Here we will pick which button to show based on if user is logged in or not, if they are lagged out in show login button, else show logout button
     var buttonToShow = !singletonUserContext.user ? // basically if (singltonUserContext.user)
-        <Button onClick={signInWithGoogle}>Login With Google</Button> : // use this button
-        [<Button key={uuidv4()} onClick={logout}>Logout</Button>, <Button key={uuidv4()} onClick={() => { navigate("/ChatroomsPage") }}>Go to Chatroom List</Button>];   // else use this button
+        <Button
+            onClick={signInWithGoogle}>Login With Google</Button> : // use this button
+        [<Button key={uuidv4()}
+            sx={{
+                borderRadius: '5em',
+                backgroundColor: '#903487',
+                mixBlendMode: 'multiply',
+                backdropFilter: 'blur(10px)',
+                boxShadow: "0px 5px 10px rgba(0, 0, 0, 0.7)"
+            }}
+            onClick={logout}>Logout</Button>, <Button key={uuidv4()} onClick={() => { navigate("/ChatroomsPage") }}>Go to Chatroom List</Button>];   // else use this button
 
     console.log(getChatrooms());
 
     return (<>
         <Background useVignette={true} useBlur={true} gifPath="/gif/cyberPunkCity.gif">
             <Stack>
-            <Box
-            sx={{
-                backgroundImage: `url("/svg/ZenCafeVerticalLogo.svg")`, // Load background image
-                backgroundSize: 'contain', // Scale the background image to fit within the container while preserving its aspect ratio
-                backgroundRepeat: 'no-repeat',
-                width: '90vh', // Set the width of the container
-                height: '90vw', // Automatically adjust the height based on the aspect ratio
-                zIndex: 1
-            }}
-        />
+                <ZenCafeLogo vh={35} />
                 <Typography>Login/Signup</Typography>
                 {buttonToShow}
             </Stack>
