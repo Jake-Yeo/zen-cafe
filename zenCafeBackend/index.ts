2ef4b9bd54a9ebb804c615d5ceafa21b79cd4814
Jake Yeo <jyeo03@students.cs.ubc.ca>
2024-08-19 15:14:33 -0700

    Added intex.ts to the backend. Used .env file to store api keys.

---
+++ zenCafeBackend/index.ts
@@ -0,0 +1,42 @@
+const express = require('express');
+import mongoose from 'mongoose';
+const app = express();
+
+require('dotenv').config();
+
+const port = process.env.PORT;
+const mongoUri = process.env.MONGO_URI;
+
+app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.get('/', (req, res) => { // get method
+    res.send("Hello from node api");
+});
+
+const productRoute = require('./routes/productRoute');
+app.use('/api/products', productRoute);
+
+import chatroomRouter from './routes/chatroomRoute';
+app.use('/chatrooms', chatroomRouter);
+
+import userRouter from './routes/userRoute';
+app.use('/users', userRouter);
+
+
+// cannot push this file because this has api key!
+mongoose.connect(mongoUri)
+    .then(() => {
+        console.log("connected to mongodb")
+        app.listen(port, () => {
+            console.log("running on port ", port);
+        });
+    })
+    .catch(() => { console.log("connection to mongodb failed") });
+
+export { }
