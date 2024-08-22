1af8b06f3c2436896289f2ebdb5e492fda88c48b
Jake Yeo <jyeo03@students.cs.ubc.ca>
2024-08-22 16:18:33 -0700

    Added debug statements, attempting to fix encryption utf-8 malformation error.

--- src/functions/envVars.ts
+++ src/functions/envVars.ts
@@ -5,4 +5,9 @@ const zenCafeApiKey = process.env.REACT_APP_ZEN_CAFE_API_KEY;
 const zenCafeEncryptionKey = process.env.REACT_APP_ZEN_CAFE_ENCRYPTION_KEY;
 export const encryptedZenCafeApiKey = CryptoJS.AES.encrypt(`${zenCafeApiKey}`, `${zenCafeEncryptionKey}`).toString();
 
+console.log("zenCafeApiUrl: ", zenCafeApiUrl);
+console.log("zenCafeApiKey: ", zenCafeApiKey);
+console.log("zenCafeEncryptionKey: ", zenCafeEncryptionKey);
+console.log("encryptedZenCafeApiKey: ", encryptedZenCafeApiKey);
+
 //https://stackoverflow.com/questions/53237293/react-evironment-variables-env-return-undefined very important
--- zenCafeBackend/controllers/authorization.ts
+++ zenCafeBackend/controllers/authorization.ts
@@ -8,6 +8,14 @@ const encryptionKey = process.env.ENCRYPTION_KEY;
 export function authorize(req: any): boolean {
     const authorizationHeader = req.headers.authorization;
     const encryptedZenCafeApiKey = authorizationHeader; // No need for "Bearer"
+
+    console.log("apiKey: ", apiKey);
+    console.log("encryptionKey: ", encryptionKey);
+    console.log("encryptedZenCafeApiKey: ", encryptedZenCafeApiKey);
+
     const decryptedApiKey = CryptoJS.AES.decrypt(encryptedZenCafeApiKey, `${encryptionKey}`).toString(CryptoJS.enc.Utf8);
+
+    console.log("decryptedApiKey: ", decryptedApiKey);
+
     return decryptedApiKey == apiKey;
 }
